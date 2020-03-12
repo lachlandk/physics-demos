6f9e1e6faeada2ecddc78ebb1e0b50a8856a03a1
lachlandk <lachlandufort-kennett@outlook.com>
2020-03-12 11:10:14 +0000

    Added loading demo metadata from JSON file

--- load-thumbnails.js
+++ load-thumbnails.js
@@ -1,6 +1,31 @@
 (function(){
-	$.getJSON("https://raw.githubusercontent.com/lachlandk/physics-demos/master/demos.json", function(data){
-		const demos = JSON.parse(data);
-		console.log(demos);
+	$.getJSON("https://raw.githubusercontent.com/lachlandk/physics-demos/master/demos.json", function(demos){
+		Object.keys(demos).forEach(function(key){
+			const demo = demos[key];
+
+			let image = document.createElement("img");
+			image.setAttribute("src", demo["thumbnail"] ? demo["thumbnail"] : "blank.png");
+			image.setAttribute("alt", "Thumbnail");
+			image.setAttribute("width", "854");
+			image.setAttribute("height", "480");
+			let title = document.createElement("h3");
+			title.innerText = demo["title"];
+			let description = document.createElement("p");
+			description.innerText = demo["description"];
+
+			let anchor = document.createElement("a");
+			anchor.setAttribute("href", "https://lachlandk.github.io/physics-demos/view?demo=" + key);
+			anchor.appendChild(image);
+			anchor.appendChild(title);
+			anchor.appendChild(description);
+
+			let article = document.createElement("article");
+			article.setAttribute("class", "thumbnail");
+			let container = document.createElement("div");
+			container.setAttribute("class", "thumbnail-container");
+			container.appendChild(anchor);
+			article.appendChild(container);
+			document.getElementById("demos").appendChild(article);
+		})
 	});
 })();

2c9405a00f3d06bfade1834760e0ce8988491728
lachlandk <lachlandufort-kennett@outlook.com>
2021-03-15 23:04:52 +0000

    Added MASSIVE performance gains

--- scripts/equationBackground.js
+++ scripts/equationBackground.js
@@ -3,14 +3,13 @@ function EquationObject(equationJSON) {
 	this.description = equationJSON.description;
 	this.wikipediaLink = equationJSON.wikipediaLink;
 
-	this.element = document.createElement("div");
-	this.element.innerText = `\\[${equationJSON.latex}\\]`;
-	this.element.style.position = "absolute";
-	this.element.style.visibility = "none";
-	equationBackgroundContainer.appendChild(this.element);
+	this.container = document.createElement("div");
+	this.container.innerText = `\\[${equationJSON.latex}\\]`;
+	this.container.style.position = "absolute";
+	this.container.style.visibility = "none";
+	equationBackgroundContainer.appendChild(this.container);
 
-	this.mouseOver = false;
-	this.pos = {
+	this.initialPos = {
 		x: Math.random() * equationBackgroundContainer.offsetWidth, // pixels, measured from top to bottom, left to right
 		y: Math.random() * equationBackgroundContainer.offsetHeight,
 		theta: Math.random() * 90 - 45 // degrees, measured clockwise relative to the x-axis
@@ -20,68 +19,117 @@ function EquationObject(equationJSON) {
 		Vy: Math.random() * 200 - 100,
 		Vtheta: Math.random() * 10 - 5  // degrees per second
 	};
-	this.bounds = {
-		xMax: equationBackgroundContainer.offsetWidth,
-		yMax: equationBackgroundContainer.offsetHeight
-	};
+	this.velocity.magnitude = Math.sqrt(this.velocity.Vx**2 + this.velocity.Vy**2);
 
 	this.initDisplay = () => {
-		const svg = this.element.childNodes[0].childNodes[0];
+		this.subContainer = this.container.childNodes[0];
+		this.svg = this.subContainer.childNodes[0];
 
 		const scale = Math.random() + 1;
-		this.element.style.width = scale * svg.width.baseVal.value + "px";
-		this.element.style.height = scale * svg.height.baseVal.value + "px";
-		svg.setAttribute("width", "100%");
-		svg.setAttribute("height", "100%");
+		this.container.style.width = scale * this.svg.width.baseVal.value + "px";
+		this.container.style.height = scale * this.svg.height.baseVal.value + "px";
+		this.svg.setAttribute("width", "100%");
+		this.svg.setAttribute("height", "100%");
 
-		this.element.childNodes[0].style.margin = "0";
-		this.element.style.left = `-${this.element.offsetWidth / 2}px`;
-		this.element.style.top = `-${this.element.offsetHeight / 2}px`;
-		this.element.style.visibility = "visible";
+		this.subContainer.style.margin = "0";
+		this.container.style.left = `-${this.container.offsetWidth / 2}px`;
+		this.container.style.top = `-${this.container.offsetHeight / 2}px`;
+		this.container.style.visibility = "visible";
 
-		this.element.addEventListener("mouseenter", this.mouseEnter);
-		this.element.addEventListener("mouseleave", this.mouseLeave);
+		this.subContainer.addEventListener("mouseenter", this.mouseEnter);
+		this.subContainer.addEventListener("mouseleave", this.mouseLeave);
+
+		this.translationAnimation = this.startTranslationAnimation();
+		this.rotationAnimation = this.startRotationAnimation();
 	}
 
-	this.animateStep = (dtSeconds) => {
-		if (!this.mouseOver) {
-			this.pos.theta += this.velocity.Vtheta * dtSeconds;
-			if (this.pos.theta < -45 || this.pos.theta > 45) {
-				this.velocity.Vtheta *= -1;
-			}
-
-			this.pos.x += this.velocity.Vx * dtSeconds;
-			this.pos.y += this.velocity.Vy * dtSeconds;
-			if (this.pos.x < 0 && this.velocity.Vx < 0) {
-				this.velocity.Vx *= -1;
-			} else if (this.pos.x > this.bounds.xMax && this.velocity.Vx > 0) {
-				this.velocity.Vx *= -1;
-			}
-			if (this.pos.y < 0 && this.velocity.Vy < 0) {
-				this.velocity.Vy *= -1;
-			} else if (this.pos.y > this.bounds.yMax && this.velocity.Vy > 0) {
-				this.velocity.Vy *= -1;
-			}
+	this.getCurrentAngle = () => {
+		let matrix = window.getComputedStyle(this.subContainer).transform
+		if (matrix !== "none") {
+			matrix = matrix.split("(")[1].split(")")[0].split(",");
+			return Math.asin(matrix[1]) * (180 / Math.PI);
 		} else {
-			// check if the object is rotating away from or towards zero
-			if (this.pos.theta < 0 && this.velocity.Vtheta < 0) {
-				this.velocity.Vtheta *= -1;
-
-			} else if (this.pos.theta > 0 && this.velocity.Vtheta > 0) {
-				this.velocity.Vtheta *= -1;
-			}
-			// TODO: replace with proper easing function
-			this.pos.theta += Math.abs(this.pos.theta) * this.velocity.Vtheta * dtSeconds;
+			return 0;
 		}
-		this.element.style.transform = `translate(${this.pos.x}px, ${this.pos.y}px) rotate(${this.pos.theta}deg)`;
+	}
+
+	this.startRotationAnimation = () => {
+		const direction = Math.sign(this.velocity.Vtheta);
+		const rotationAnimation = this.subContainer.animate([
+			{transform: `rotate(${this.initialPos.theta}deg)`},
+			{transform: `rotate(${45 * direction}deg)`}
+		], {
+			duration: 1000 * (Math.abs(45 * direction - this.initialPos.theta) / Math.abs(this.velocity.Vtheta)),
+			fill: "forwards"
+		});
+		rotationAnimation.onfinish = () => {
+			this.initialPos.theta = 45 * direction;
+			this.velocity.Vtheta *= -1;
+			this.rotationAnimation = this.startRotationAnimation();
+		}
+		return rotationAnimation;
+	}
+
+	this.startTranslationAnimation = () => {
+		let yBoundaryPos;
+		let xBoundaryPos;
+		const gradient = this.velocity.Vy / this.velocity.Vx;
+		const inverse_gradient = this.velocity.Vx / this.velocity.Vy;
+		if (this.velocity.Vx < 0) {
+			yBoundaryPos = this.initialPos.y - gradient * this.initialPos.x;
+		} else {
+			yBoundaryPos = this.initialPos.y + gradient * (equationBackgroundContainer.offsetWidth - this.initialPos.x);
+		}
+		if (yBoundaryPos < 0) {
+			yBoundaryPos = 0;
+			xBoundaryPos = this.initialPos.x - inverse_gradient * this.initialPos.y;
+			this.velocity.Vy *= -1;
+		} else if (yBoundaryPos > equationBackgroundContainer.offsetHeight) {
+			yBoundaryPos = equationBackgroundContainer.offsetHeight;
+			xBoundaryPos = inverse_gradient * (equationBackgroundContainer.offsetHeight - this.initialPos.y) + this.initialPos.x;
+			this.velocity.Vy *= -1;
+		} else {
+			this.velocity.Vx > 0 ? xBoundaryPos = equationBackgroundContainer.offsetWidth : xBoundaryPos = 0;
+			this.velocity.Vx *= -1;
+		}
+		const totalDistance = Math.sqrt(Math.abs(this.initialPos.x - xBoundaryPos)**2 + Math.abs(this.initialPos.y - yBoundaryPos)**2);
+		const translationAnimation = this.container.animate([
+			{transform: `translate(${this.initialPos.x}px, ${this.initialPos.y}px)`},
+			{transform: `translate(${xBoundaryPos}px, ${yBoundaryPos}px)`}
+		], {
+			duration: 1000 * (totalDistance / this.velocity.magnitude),
+			fill: "forwards"
+		});
+		translationAnimation.onfinish = () => {
+			this.initialPos.x = xBoundaryPos;
+			this.initialPos.y = yBoundaryPos;
+			this.translationAnimation = this.startTranslationAnimation();
+		}
+		return translationAnimation;
 	}
 
 	this.mouseEnter = () => {
-		this.mouseOver = true;
+		this.velocity.Vtheta *= -1;
+		this.initialPos.theta = this.getCurrentAngle();
+		this.rotationAnimation.cancel();
+		this.translationAnimation.pause();
+		this.rotationAnimation = this.subContainer.animate([
+			{transform: `rotate(${this.initialPos.theta}deg)`},
+			{transform: `rotate(0deg)`}
+		], {
+			duration: 1000,
+			easing: "ease-out"
+		});
+		this.rotationAnimation.onfinish = () => {
+			this.initialPos.theta = 0;
+		}
 	}
 
 	this.mouseLeave = () => {
-		this.mouseOver = false;
+		this.initialPos.theta = this.getCurrentAngle();
+		this.rotationAnimation.cancel();
+		this.rotationAnimation = this.startRotationAnimation();
+		this.translationAnimation.play();
 	}
 }
 
@@ -98,14 +146,6 @@ Promise.all([
 	equationBackgroundContainer.style.width = "100%";
 	equationBackgroundContainer.style.height = "100%";
 	document.body.appendChild(equationBackgroundContainer);
-	window.addEventListener("resize", () => {
-		equationObjects.forEach(equationObject => {
-			equationObject.bounds = {
-				xMax: equationBackgroundContainer.offsetWidth,
-				yMax: equationBackgroundContainer.offsetHeight
-			};
-		});
-	});
 
 	const equationObjects = [];
 	results[0].equations.forEach(equationJSON => {
@@ -115,19 +155,9 @@ Promise.all([
 	MathJax.typeset();
 	equationObjects.forEach(equationObject => {equationObject.initDisplay();});
 
-	let previousTimestamp;
-	const animateEquationObjects = (currentTimestamp) => {
-		let dt;
-		if (!previousTimestamp || currentTimestamp - previousTimestamp > 1000) {
-			dt = 0;
-		} else {
-			dt = currentTimestamp - previousTimestamp;
-		}
-		equationObjects.forEach(equationObject => {
-			equationObject.animateStep(dt/1000);
-		});
-		previousTimestamp = currentTimestamp;
-		window.requestAnimationFrame(animateEquationObjects);
-	}
-	window.requestAnimationFrame(animateEquationObjects);
+	// Imperfections
+	// - No immediate reaction if the page size is changed
+	// - Elements that are on top of each other can "steal" mouse events
+	// - Occasionally when javascript is busy there can be a small delay on bounce
+	// 		- Could we append keyframes to existing animations? This would be better for calculating the wall bounces in advance
 });
